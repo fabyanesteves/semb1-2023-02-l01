d9633a27d588ee87625441ffb7e30638c9fd63d9
fabyanesteves <159019224+fabyanesteves@users.noreply.github.com>
2024-02-27 20:15:56 -0300

    Update main.c

--- stm32f411-blackpill/src/main.c
+++ stm32f411-blackpill/src/main.c
@@ -1,30 +1,15 @@
-/****************************************************************************
- * main.c
- *
- *   Authors: Daniel Pereira de Carvalho <daniel.carvalho@ufu.br>
- *            Marcelo Barros de Almeida  <marcelo.barros@ufu.br>
- *
- ****************************************************************************/
-
-/****************************************************************************
- * Included Files
- ****************************************************************************/
-
+/*
+ Authors: Daniel Pereira de Carvalho <daniel.carvalho@ufu.br>
+           Marcelo Barros de Almeida  <marcelo.barros@ufu.br>
+*/
+  
 #include <stdlib.h>
 
-/****************************************************************************
- * Public Functions
- ****************************************************************************/
-
-/****************************************************************************
- * Name: main
- ****************************************************************************/
-
-int main(void)
+int main(int argc, char *argv[])
 {
-  while(1);
+  while(1){};
 
-  /* Nao deveria chegar aqui */
+  /* Nao deveria chegar aqui.*/
 
   return EXIT_SUCCESS;
 }
